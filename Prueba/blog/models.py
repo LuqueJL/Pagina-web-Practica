a768628d6d4aa017440c317082e77c317c8777b0
Jesus Luques <enriqueluques11@gmail.com>
2024-04-15 15:01:44 -0400

    V1

---
+++ Prueba/blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class post(models.Model):
+
+    class status(models.TextChoices):
+        draft = 'DF', 'Draft'
+        published = 'PB', 'published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=status.choices, default=status.draft)
+    
+    class Meta:
+        ordering = ['-publish']
+        indexes = [models.Index(fields=['-publish']),]
+
+    def __str__(self):
+        return self.title
